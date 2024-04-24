2d0d62c269285d9e71828d578a0ffb390f516853
Hiroto1403 <113161371+Hiroto1403@users.noreply.github.com>
2024-04-24 15:20:19 +0900

    ステージ確認

---
+++ Assets/Itihara/sqript/Destroy.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    //Õ“Ë‚µ‚½Aobject”j‰ó
+      void OnCollisionEnter(Collision collision)
+    {
+        //playerƒ^ƒO‚ªğŒ
+        if (collision.gameObject.tag == "Player")
+        {
+            //ˆê•bŒãÁ‚¦‚é
+            Destroy(gameObject,1.0f);
+        }
+    }
+}
