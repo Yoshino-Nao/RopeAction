a619b84b59f6be75870ba22be1f7084de8e95de5
rinne9029 <0kt4x34y2ky637r@au.com>
2024-05-01 12:10:57 +0900

    追加
    周辺からロープを付けられるオブジェクトを検知するスクリプト

--- Assets/Nakata/Sqript/PlayerRayCast.cs
+++ Assets/Nakata/Sqript/PlayerRayCast.cs
@@ -2,40 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Linq;
+
 public class PlayerRayCast : MonoBehaviour
 {
-    private float MinLength=10;
-    private RaycastHit obj;
+    //ロープをつけられるオブジェクトまでの一番短い距離
+    private float MinLength=999;
+    //ロープをつけられるオブジェクトの検知フラグ
+    private bool Checkflag = false;
+    private GameObject obj;
     void Update()
     {
-        Explosion();
+        if(Explosion().name == "Enemy")
+        {
+            Debug.Log("ロープをつけられる場所あります。");
+            
+        }
+        else
+        {
+            Debug.Log("ロープをつけられる場所ありません。");
+        }
+        
     }
 
-    private RaycastHit Explosion()
+    //範囲内から一番近いオブジェクトを検索する関数
+    public GameObject Explosion()
     {
-        RaycastHit[] hits = Physics.SphereCastAll(
+        var hits = Physics.SphereCastAll(
             transform.position,     //中心
             5.0f,                   //半径
-            Vector3.forward);       //方向
+            Vector3.forward).Select(h => h.transform.gameObject).ToList();    //方向
 
-        Debug.Log($"検出されたコライダーの数{hits.Length}");
+        //Debug.Log($"検出されたコライダーの数{hits.Length}");
 
         foreach (var hit in hits)
         {
-            //距離を求める
-            float Length = Vector3.Distance(transform.position, hit.transform.position);
-            //距離が短いなら
-            if(MinLength>Length)
+            //レイに接触したオブジェクトの名前がEnemyの時
+            if (hit.name == "Enemy")
             {
-                //最短距離を更新
-                MinLength = Length;
-                //一番短い距離のオブジェクトに更新する
+                //エネミー検知フラグ
+                Checkflag = true;
+                //距離を求める
+                float Length = Vector3.Distance(transform.position, hit.transform.position);
+                //距離が短いなら
+                if (MinLength > Length)
+                {
+                    //最短距離を更新
+                    MinLength = Length;
+                    //オブジェにエネミーを返す
+                    obj = hit;
+                }
+            }
+            //エネミーを一度も検知しなければ
+            else if(!Checkflag)
+            {
+                //エネミー以外のオブジェクトを返す
                 obj = hit;
             }
         }
         //距離リセット
-        Debug.Log($"一番短い距離のオブジェクト{obj.collider.name}{MinLength}");
-        MinLength = 10;
+        MinLength = 999;
+        //フラグリセット
+        Checkflag = false;
+        
         return obj;
+        
     }
 }
