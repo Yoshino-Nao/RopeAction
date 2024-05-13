6e5fe39220585d847e3dbe0c78ea13cf01a95204
YoshinoNao <113161290+Yoshino-Nao@users.noreply.github.com>
2024-05-13 09:33:15 +0900

    UIを臨時修正

--- Assets/Nakata/Sqript/UITest.cs
+++ Assets/Nakata/Sqript/UITest.cs
@@ -8,28 +8,36 @@ public class UITest : MonoBehaviour
     //帯をつけられるオブジェクト
     public Transform m_attachTf;
     public GameObject m_Canvas;
-    public RectTransform Image;
+    public RectTransform rectTf;
+    public Image image;
     Vector3 view;
     void Start()
     {
-        
+
     }
     void Update()
     {
-        //ワールド座標からビューポート座標に変換
-        view = Camera.main.WorldToViewportPoint(m_attachTf.position);
-        //ビューポート座標からスクリーン座標に変換
-        Image.position = Camera.main.ViewportToScreenPoint(view);
-        Debug.Log(Image.position);
+        if (m_attachTf != null)
+        {
+            //ワールド座標からビューポート座標に変換
+            view = Camera.main.WorldToViewportPoint(m_attachTf.position);
+        }
 
-        if(Image.position.z<0)
-        {
-            m_Canvas.SetActive(false);
-        }
-        else
-        {
-            m_Canvas.SetActive(true);
-        }
+
+        image.enabled = view.z > 0 && m_attachTf;
+
+        //ビューポート座標からスクリーン座標に変換
+        rectTf.position = Camera.main.ViewportToScreenPoint(view);
+        Debug.Log(rectTf.position);
+
+        //if(Image.position.z<0)
+        //{
+        //    m_Canvas.SetActive(false);
+        //}
+        //else
+        //{
+        //    m_Canvas.SetActive(true);
+        //}
 
     }
 }
