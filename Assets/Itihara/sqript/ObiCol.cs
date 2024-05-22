8957844cf1d0341dac672672bc1681353686963d
Hiroto1403 <113161371+Hiroto1403@users.noreply.github.com>
2024-05-22 15:12:44 +0900

    ギミック完成、ステージ２

--- Assets/Itihara/sqript/ObiCol.cs
+++ Assets/Itihara/sqript/ObiCol.cs
@@ -25,49 +25,51 @@ public class ObiCol : MonoBehaviour
     {
         solver.OnCollision -= Solver_OnCollision;
     }
+
     private void Solver_OnCollision(ObiSolver s, ObiSolver.ObiCollisionEventArgs e)
     {
         var world = ObiColliderWorld.GetInstance();
         foreach (Oni.Contact contact in e.contacts)
         {
             var col = world.colliderHandles[contact.bodyB].owner;
-            if (col != null)
+            if (col != null )
             {
                 var fallObject = col.GetComponent<fallObject>();
                 if (fallObject != null)
-                    fallObject.Fall();
+                    fallObject.IsRopeUnderTension();
             }
         }
     }
-    public ObiRope rope;
-    public float tensionThreshold = 10.0f; // ˆø‚Á’£‚è‚Æ”»’è‚·‚é—Í‚Ìè‡’l
-    public Rigidbody targetRigidbody; // ‘€ì‘ÎÛ‚ÌRigidbody
+    //public ObiRope rope;
+    //public float tensionThreshold = 10.0f; // ˆø‚Á’£‚è‚Æ”»’è‚·‚é—Í‚Ìè‡’l
+    //public Rigidbody targetRigidbody; // ‘€ì‘ÎÛ‚ÌRigidbody
+    //private float time = 0.0f;
+    //private bool check;
 
+    //bool IsRopeUnderTension()
+    //{
+    //    time += UnityEngine.Time.deltaTime;
+    //    var solver = rope.solver;
+    //    float tension = rope.CalculateLength() / rope.restLength - 1;
+    //    //DebugPrint.Print(string.Format("{0}", tension));
+
+    //    int particleIndex = rope.solverIndices[i];
+    //    if (tension > tensionThreshold)
+    //    {
+    //        time += UnityEngine.Time.deltaTime;
+    //        DebugPrint.Print(string.Format("{0}", time));
+    //        if (time >= 0.5f)
+    //            check = true;
+    //        if (check)
+    //            return true;
+    //    }
+
+    //    return false;
+    //}
     void Update()
     {
-        if (IsRopeUnderTension())
-        {
-            if (targetRigidbody != null)
-            {
-                targetRigidbody.isKinematic = false;
-                Debug.Log("Rope is under tension! isKinematic is set to false.");
-            }
-        }
-    }
-
-    bool IsRopeUnderTension()
-    {
-        var solver = rope.solver;
-
-        for (int i = 0; i < rope.activeParticleCount; i++)
-        {
-            int particleIndex = rope.solverIndices[i];
-            Vector3 force = solver.externalForces[particleIndex];
-            if (force.magnitude > tensionThreshold)
-            {
-                return true;
-            }
-        }
-        return false;
+       
+       
+           
     }
 }
--- Assets/Itihara/sqript/fallObject.cs
+++ Assets/Itihara/sqript/fallObject.cs
@@ -1,10 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Obi;
 
 public class fallObject : MonoBehaviour
 {
-   
+    public ObiRope rope;
+    public float tensionThreshold = 10.0f; // 引っ張りと判定する力の閾値
+    public Rigidbody targetRigidbody; // 操作対象のRigidbody
+    private float time = 0.0f;
+    private bool check;
+    public float WaitTime = 3.0f;
+
+   public void  IsRopeUnderTension()
+    {
+        time += UnityEngine.Time.deltaTime;
+        var solver = rope.solver;
+        float tension = rope.CalculateLength() / rope.restLength - 1;
+        //DebugPrint.Print(string.Format("{0}", tension));
+        if (tension > tensionThreshold)
+        {
+            time += UnityEngine.Time.deltaTime;
+            DebugPrint.Print(string.Format("{0}", time));
+            if (time >= WaitTime)
+                check = true;
+            if (check)
+                body.isKinematic = false;
+        }
+
+       
+    }
+    //[SerializeField]
+    //private struct NoiseParam
+    //{
+
+    //    //振動
+    //    public float amplitude;
+
+    //    //振動の速さ
+    //    public float speed;
+
+    //    //パーリンノイズのオフセット
+    //    [NonSerialized] public float offset;
+
+    //    //乱数のオフセット値を指定する
+    //    public void SetRandomOffset()
+    //    {
+    //        offset = UnityEngine.Random.Range(0f, 256f);
+    //    }
+
+    //    //指定時刻のパーリンノイズ値を取得
+    //}
     Rigidbody body;
 
     private void Start()
@@ -20,8 +67,7 @@ public class fallObject : MonoBehaviour
             body.isKinematic = true;
         }
     }
-    public void Fall()
-    {
-        body.isKinematic = false;
-    }
+    
+      
+ 
 }
