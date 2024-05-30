11fcd31003b738f13483cf34be58cd31e383c408
YoshinoNao <113161290+Yoshino-Nao@users.noreply.github.com>
2024-05-30 13:40:34 +0900

    2Dカメラ入力のバグを修正

--- Assets/Yoshino/Scripts/CameraControllerOn2D.cs
+++ Assets/Yoshino/Scripts/CameraControllerOn2D.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
@@ -8,17 +6,23 @@ public class CameraController2D : MonoBehaviour
     private CinemachineVirtualCamera m_virtualCamera;
     private CinemachineTransposer m_transposer;
     private CinemachineComposer m_composer;
-    private Vector3 m_offset;
+    enum eInputMode
+    {
+        Mouse,
+        KeyArrow
+    }
+    [SerializeField] private eInputMode m_mode = eInputMode.Mouse;
     [SerializeField] private Vector3 m_defOffset;
-    [SerializeField] private float m_pow = 3;
+    [SerializeField] private float m_comMoveSpeed = 3;
+    [SerializeField] private float m_moveToDefSpeed;
     [SerializeField] private float m_maxLength = 3;
+    [SerializeField] bool test;
     // Start is called before the first frame update
     void Start()
     {
         m_virtualCamera = GetComponent<CinemachineVirtualCamera>();
         m_transposer = m_virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
         m_composer = m_virtualCamera.GetCinemachineComponent<CinemachineComposer>();
-        m_offset = m_defOffset;
         //初期値
         m_transposer.m_FollowOffset = m_defOffset;
         m_composer.m_TrackedObjectOffset = new Vector3(0, m_defOffset.y, 0);
@@ -27,22 +31,47 @@ public class CameraController2D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Mouse X");
-        float v = Input.GetAxis("Mouse Y");
-        Vector3 Value = new Vector3(h, v, 0) * m_pow;
-        //DebugPrint.Print(string.Format("{0}", new Vector2(h, v)));
-
-        //m_transposer.m_FollowOffset += Value;
-        //if ((m_transposer.m_FollowOffset- m_defOffset).magnitude >= m_maxLength) 
-        //{
-        //    m_transposer.m_FollowOffset = Vector3.ClampMagnitude(m_transposer.m_FollowOffset, m_maxLength);
-        //    m_transposer.m_FollowOffset.z = m_defOffset.z;
-        //}
-        //DebugPrint.Print(string.Format("{0}{1}", (m_transposer.m_FollowOffset - m_defOffset).magnitude, m_maxLength));
-        m_composer.m_TrackedObjectOffset += Value;
-        if (Vector3.Distance(m_composer.m_TrackedObjectOffset, new Vector3(0, m_defOffset.y, 0)) >= m_maxLength)
+        float h;
+        float v;
+        if (m_mode == eInputMode.Mouse)
         {
-            m_composer.m_TrackedObjectOffset = Vector3.ClampMagnitude(m_composer.m_TrackedObjectOffset, m_maxLength);
+            h = Input.GetAxis("Mouse X");
+            v = Input.GetAxis("Mouse Y");
+        }
+        else
+        {
+            h = Input.GetAxis("HorizontalArrow");
+            v = Input.GetAxis("VerticalArrow");
+        }
+        Vector3 Value = new Vector3(h, v, 0);
+
+        ////入力がない場合は初期位置に補間する
+        if (Value.magnitude > 0)
+        {
+            if (test)
+            {
+                m_transposer.m_FollowOffset += Value * m_comMoveSpeed;
+            }
+            m_composer.m_TrackedObjectOffset += Value * m_comMoveSpeed;
+        }
+        else
+        {
+            m_transposer.m_FollowOffset = Vector3.MoveTowards(m_transposer.m_FollowOffset, m_defOffset, m_moveToDefSpeed);
+            m_composer.m_TrackedObjectOffset = Vector3.MoveTowards(m_composer.m_TrackedObjectOffset, new Vector3(0, m_defOffset.y, 0), m_moveToDefSpeed);
+        }
+        //限界の距離以上は動かないようにする
+        {
+            Vector2 pos = new Vector2(m_transposer.m_FollowOffset.x, m_transposer.m_FollowOffset.y);
+            //Vector2で長さをクランプする
+            Vector2 ClampVec = Vector2.ClampMagnitude(
+                pos - new Vector2(m_defOffset.x, m_defOffset.y), m_maxLength);
+            m_transposer.m_FollowOffset = new Vector3(ClampVec.x, ClampVec.y + m_defOffset.y, m_defOffset.z);
+        }
+        {
+            Vector2 pos = new Vector2(m_composer.m_TrackedObjectOffset.x, m_composer.m_TrackedObjectOffset.y);
+            Vector2 ClampVec = Vector2.ClampMagnitude(
+                 pos - new Vector2(0, m_defOffset.y), m_maxLength);
+            m_composer.m_TrackedObjectOffset = new Vector3(ClampVec.x, ClampVec.y + m_defOffset.y, 0);
         }
     }
 }
