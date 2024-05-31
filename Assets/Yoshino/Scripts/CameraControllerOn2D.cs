c0cefeed09bc466233cecc08df9c2c665c2d10d7
YoshinoNao <113161290+Yoshino-Nao@users.noreply.github.com>
2024-05-31 11:16:49 +0900

    2dカメラのバグを修正

--- Assets/Yoshino/Scripts/CameraControllerOn2D.cs
+++ Assets/Yoshino/Scripts/CameraControllerOn2D.cs
@@ -5,7 +5,7 @@ public class CameraController2D : MonoBehaviour
 {
     private CinemachineVirtualCamera m_virtualCamera;
     private CinemachineTransposer m_transposer;
-    private CinemachineComposer m_composer;
+    private CinemachinePOV m_pov;
     enum eInputMode
     {
         Mouse,
@@ -14,21 +14,28 @@ public class CameraController2D : MonoBehaviour
     [SerializeField] private eInputMode m_mode = eInputMode.Mouse;
     [SerializeField] private Vector3 m_defOffset = new Vector3(0, 1.5f, -7);
     [SerializeField] private float m_comMoveSpeed = 1;
+    [SerializeField] private float m_camRotateAccelTime = 0.5f;
     [SerializeField] private float m_moveToDefSpeed = 0.25f;
     [SerializeField] private float m_maxLength = 3;
     //カメラの角度のみを動かす
     [SerializeField] bool m_isOnlyRotation = false;
     //操作していない場合、カメラの位置をデフォルトに戻そうとする
     [SerializeField] bool m_isMoveDefault = true;
+
+    private float m_povSpeedDef;
     // Start is called before the first frame update
     void Start()
     {
         m_virtualCamera = GetComponent<CinemachineVirtualCamera>();
         m_transposer = m_virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
-        m_composer = m_virtualCamera.GetCinemachineComponent<CinemachineComposer>();
+        m_pov = m_virtualCamera.GetCinemachineComponent<CinemachinePOV>();
         //初期値
         m_transposer.m_FollowOffset = m_defOffset;
-        m_composer.m_TrackedObjectOffset = new Vector3(0, m_defOffset.y, 0);
+        //m_pov.m_HorizontalAxis.m_MaxValue = m_maxLength * 10;
+        m_pov.m_VerticalAxis.m_AccelTime = m_pov.m_HorizontalAxis.m_AccelTime = m_comMoveSpeed * 10;
+
+        m_povSpeedDef = m_pov.m_HorizontalAxis.m_MaxSpeed;
+        m_pov.m_HorizontalAxis.m_AccelTime = m_pov.m_VerticalAxis.m_AccelTime = m_camRotateAccelTime;
     }
 
     // Update is called once per frame
@@ -40,27 +47,42 @@ public class CameraController2D : MonoBehaviour
         {
             h = Input.GetAxis("Mouse X");
             v = Input.GetAxis("Mouse Y");
+            m_pov.m_HorizontalAxis.m_InputAxisName = "Mouse X";
+            m_pov.m_VerticalAxis.m_InputAxisName = "Mouse Y";
+
         }
         else
         {
             h = Input.GetAxis("HorizontalArrow");
             v = Input.GetAxis("VerticalArrow");
+            m_pov.m_HorizontalAxis.m_InputAxisName = "HorizontalArrow";
+            m_pov.m_VerticalAxis.m_InputAxisName = "VerticalArrow";
         }
         Vector3 Value = new Vector3(h, v, 0);
 
         ////入力がない場合は初期位置に補間する
         if (Value.magnitude > 0)
         {
-            if (!m_isOnlyRotation)
+            if (m_isOnlyRotation)
+            {
+                m_pov.m_HorizontalAxis.m_MaxSpeed = m_povSpeedDef;
+                m_pov.m_VerticalAxis.m_MaxSpeed = m_povSpeedDef;
+            }
+            else
             {
                 m_transposer.m_FollowOffset += Value * m_comMoveSpeed;
+                m_pov.m_HorizontalAxis.m_MaxSpeed = 0;
+                m_pov.m_VerticalAxis.m_MaxSpeed = 0;
+
             }
-            m_composer.m_TrackedObjectOffset += Value * m_comMoveSpeed;
+            //m_pov.m_HorizontalAxis.Value += Value.x * (m_povMaxValueX * m_comMoveSpeed);
+            //m_pov.m_VerticalAxis.Value += Value.y * m_comMoveSpeed;
         }
         else if (m_isMoveDefault)
         {
             m_transposer.m_FollowOffset = Vector3.MoveTowards(m_transposer.m_FollowOffset, m_defOffset, m_moveToDefSpeed);
-            m_composer.m_TrackedObjectOffset = Vector3.MoveTowards(m_composer.m_TrackedObjectOffset, new Vector3(0, m_defOffset.y, 0), m_moveToDefSpeed);
+            m_pov.m_HorizontalAxis.Value = Mathf.MoveTowards(m_pov.m_HorizontalAxis.Value, 0f, m_moveToDefSpeed * 10);
+            m_pov.m_VerticalAxis.Value = Mathf.MoveTowards(m_pov.m_VerticalAxis.Value, 0f, m_moveToDefSpeed * 10);
         }
         //限界の距離以上は動かないようにする
         {
@@ -71,10 +93,11 @@ public class CameraController2D : MonoBehaviour
             m_transposer.m_FollowOffset = new Vector3(ClampVec.x, ClampVec.y + m_defOffset.y, m_defOffset.z);
         }
         {
-            Vector2 pos = new Vector2(m_composer.m_TrackedObjectOffset.x, m_composer.m_TrackedObjectOffset.y);
-            Vector2 ClampVec = Vector2.ClampMagnitude(
-                 pos - new Vector2(0, m_defOffset.y), m_maxLength);
-            m_composer.m_TrackedObjectOffset = new Vector3(ClampVec.x, ClampVec.y + m_defOffset.y, 0);
+            //Vector2 pos = new Vector2(m_pov.m_TrackedObjectOffset.x, m_pov.m_TrackedObjectOffset.y);
+            //Vector2 ClampVec = Vector2.ClampMagnitude(
+            //     pos - new Vector2(0, m_defOffset.y), test);
+            //DebugPrint.Print(string.Format("pos{0}Clamp{1}", pos, ClampVec));
+            //m_pov.m_TrackedObjectOffset = new Vector3(ClampVec.x, ClampVec.y + m_defOffset.y, 0);
         }
     }
 }
