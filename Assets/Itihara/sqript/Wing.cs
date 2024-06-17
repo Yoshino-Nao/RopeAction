1cdabea9682c741a96444d9f81f56f291cd47e82
Hiroto1403 <113161371+Hiroto1403@users.noreply.github.com>
2024-06-17 12:17:36 +0900

    ステージ2見た目も含め完成
    見た目のやつの当たり判定は消した

--- Assets/Itihara/sqript/Wing.cs
+++ Assets/Itihara/sqript/Wing.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class Wing : MonoBehaviour
 {
+    public float MathfTime = 0.3f;
     Vector3 startPos;
     // Start is called before the first frame update
     void Start()
-    {
+    { 
         startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float posY = startPos.y + Mathf.Sin(Time.time) * 0.3f;
+        float posY = startPos.y + Mathf.Sin(Time.time) * MathfTime;
         transform.position = new Vector3(transform.position.x, posY, transform.position.z);
 
     }
